6fcd07b9156766c27e6bec806032b7df34e88a80
KittleCodes <170134922+KittleCodes@users.noreply.github.com>
2024-10-29 20:46:12 -0400

    Revert "Update main.py"
    This reverts commit f4ae1584cd2e7758345dc403f58b3abef47bef67.

--- main.py
+++ main.py
@@ -378,9 +378,9 @@ def verify_captcha():
     except jwt.InvalidTokenError:
         return jsonify({"success": False, "message": "Invalid token!"})
 
-
-# Create the database tables
-with app.app_context():
-    db.create_all()
-# Run the Flask app
-app.run(host='0.0.0.0', port=5007, debug=True)
+if __name__ == '__main__':
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
+    # Run the Flask app
+    app.run(host='0.0.0.0', port=5007, debug=True)
